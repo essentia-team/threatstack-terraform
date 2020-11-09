1dcd21b764f48cbb775f464c513d35b5bf806e62
Osama <osama.luhishi@essentia-analytics.com>
2020-11-09 12:21:38 +0000

    Testing

--- provider.tf
+++ provider.tf
@@ -1,5 +1,5 @@
-provider "aws" {
-  alias   = "threatstack"
-  version = "~> 3.0,!= 3.14.0"
-  region  = "eu-west-1"
-}
+//provider "aws" {
+//  alias   = "threatstack"
+//  version = "~> 3.8"
+//  region  = "eu-west-1"
+//}
